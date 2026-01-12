4c8b0e99b707b075c481520c08ce75c1568dbab7
sk8377280382-cmyk <sk8377280382@gmail.com>
2026-01-12 15:32:15 +0530

    Add organic farming program to save earth and health

---
+++ brick_labor.py
@@ -0,0 +1,43 @@
+
+#||>>>>>>>>>> This code is all about the to save the life , and to less disease , to increase this process which below are mentioned our country will be great <<<<<<<<< ||
+
+
+
+# The purpose of this program is only to save the life all the people by disease I am make a idea to try with day or night the IDEA is if use animal's manure more compare  chemical fertilizers , for every year we repeat this process than 
+
+
+#  because we use our food chemical which go in our body and became disease our body  
+# pure animals manure we grow pure crop without  using chemicals 
+    # because we all know when we burn any dry things might be a crop , or any things or other, then it produce a air pollution  this air to meet with oxygen and make harmful of environment , when we sleep in the night then we take oxygen then we taking these harmful oxygen which are very dangerous of our body.
+    
+    # So, we should grow many more tree to save our life , our environment , and our society our country .
+             # THANKU VERY MUCH TO ALL ? ? ? 
+             
+    # THIS PROGRAM's code is very simple 
+    
+    
+    ## enter the fertilizers which we used in my field
+    
+    
+    animals_manure = int(input("enter the animals_manure:"))
+    chemicals = int(input(" enter the chemicals : "))
+    
+    # to give the condition to save our earth
+    # option -> 1 
+    if animals_manure > chemicals :
+        print(" Life became saved ")
+        print(" NO produce any pollution ")
+    # option -> 2
+    elif chemicals > animals_manure:
+        print(" Disease will be produce ")
+        print(" A poor man always to live diseased")
+        print(" Beacause he earn less money so that he expenditure to grow his daily life ")
+    # option ->  3 
+    else :
+        print("the earth don't rise by disease ")
+        
+    # consoulsion is caming up if we obey the option first(1)  then we absolutely to save our life, our earth , our society , our country .
+    
+    
+    
+    
