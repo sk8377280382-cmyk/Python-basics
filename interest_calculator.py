2cc13cf37015d1186e822fe75cb3cf8451b1cceb
sk8377280382-cmyk <sk8377280382@gmail.com>
2026-01-12 17:05:08 +0530

    interest_calculator.py

---
+++ interest_calculator.py
@@ -0,0 +1,28 @@
+# this program is about the absolute beginner and more advanced learner 
+# high level interest program 
+# simple interest program 
+# IF YOU WANT OT make foundation of apptitude than call it number 
+# the number is 8534005489
+
+# si = p * r * t /100
+
+principal = int(input("enter the principal :" ))
+rate = int(input(" enter the rate :"))
+time = int(input(" enter the time :" ))
+# store all the variable / value of si 
+simple interest = principal * rate * time / 100
+
+# find the simple interest 
+
+print(simple interest )
+
+
+# short trick method which i am created here 
+
+# let suppose p = 1000 , r =10, t = 2years , find the si = ?? . 
+# jo principal hota ha bo apna aap ma 100 percent ke value rekhta ha 
+# so, we can write this now and si = rt% 
+
+#  now ->  1000*si/100
+# 1000*20/100
+# final answer is => 200 answer is correct.
