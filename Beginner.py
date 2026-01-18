8c6fd4f580100265d5dc9dcaf4af4cd3cc85da57
sk8377280382-cmyk <sk8377280382@gmail.com>
2026-01-18 11:48:38 +0530

    Create Beginner.py
    I am upload this simple program which are used in machine learning algorithms .

---
+++ Beginner.py
@@ -0,0 +1,17 @@
+
+# Wap  to check the user enter any key is a number or another.
+char = int(input(' Press any key : '))
+# condition start here
+if (char.isspace()):
+   print(' this is a space ')
+if (char.isdigit()):
+   print(' This is a digit')
+if (char.isalpha()):
+   print('this is a character ')
+
+
+# output of this program is :
+Press any key = 5
+This is a digit and so on 
+
+    
